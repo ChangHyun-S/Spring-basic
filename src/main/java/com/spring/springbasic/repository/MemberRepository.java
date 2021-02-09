4ab53651a1bbc4d0230f70221e62c919896d2f6a
ChangHyun-S <lkisa5@gmail.com>
2021-02-09 12:09:55 +0900

    Add MemberRepository...

---
+++ src/main/java/com/spring/springbasic/repository/MemberRepository.java
@@ -0,0 +1,13 @@
+package com.spring.springbasic.repository;
+
+import com.spring.springbasic.domain.Member;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface MemberRepository {
+    Member save(Member member);
+    Optional<Member> findById(Long id);
+    Optional<Member> findByName(String name);
+    List<Member> findAll();
+}

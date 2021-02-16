6abc1a892af021a475b39867c15f67a1748a27e8
ChangHyun-S <lkisa5@gmail.com>
2021-02-16 12:50:31 +0900

    Remove java bean

--- src/main/java/com/spring/springbasic/SpringConfig.java
+++ src/main/java/com/spring/springbasic/SpringConfig.java
@@ -8,16 +8,16 @@ import org.springframework.context.annotation.Configuration;
 
 // 자바 코드로 직접 스프링 빈 등록
 // 직접 등록하거나 new 생성하면 AutoWired 적용 안됨
-@Configuration
-public class SpringConfig {
-
-    @Bean
-    public MemberService memberService() {
-        return new MemberService(memberRepository());
-    }
-
-    @Bean
-    public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
-    }
-}
+//@Configuration
+//public class SpringConfig {
+//
+//    @Bean
+//    public MemberService memberService() {
+//        return new MemberService(memberRepository());
+//    }
+//
+//    @Bean
+//    public MemberRepository memberRepository() {
+//        return new MemoryMemberRepository();
+//    }
+//}
